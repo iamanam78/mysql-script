c7224256596c6cf6107d3155b4acd56d961addf9
Ubuntu <ubuntu@ip-172-31-28-202.us-east-2.compute.internal>
2020-11-25 12:43:35 +0000

    first commit

---
+++ myscript.sh
@@ -0,0 +1,23 @@
+ #!/bin/bash
+# My first script
+
+#echo "Hello World!"
+#sudo apt-get update
+
+
+
+#sudo apt-get dist-upgrade
+#sudo wget  http://dev.mysql.com/get/mysql-apt-config_0.6.0-1_all.den
+#sudo dpkg -i mysql-apt-config_0.6.0-1_all.deb
+#mysql -uroot -p
+
+MYNUM=10
+
+if [ $MYNUM -eq 100 ]
+then
+   echo "MYNUM is equal to 10"
+else 
+    echo "MYNUM is not equal to 10"
+fi
+
+
